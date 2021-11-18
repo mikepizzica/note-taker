8cbf2abef97f17e5df3dbfb3b953c0f7b866b8d9
Mike Pizzica <mike.pizzica@gmail.com>
2021-11-17 22:29:40 -0500

    Pushing to Heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const app = express();
 const path = require('path');
 const fs = require('fs');
